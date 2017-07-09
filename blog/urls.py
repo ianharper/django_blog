e9e43ab6ae2a4587e03ac2b23e0f106ab19a377c
Ian Harper <ian@ianbharper.com>
2017-07-09 13:25:08 -0700

    Changed tag list url to accept tags with spaces

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 	url(r'^category/(?P<name>[\d\w]+)/(?P<page>\d+)?$', views.category_list, name='category_list'),
-	url(r'^tag/(?P<name>[\d\w]+)/(?P<page>\d+)?$', views.tag_list, name='tag_list'),
+	url(r'^tag/(?P<name>[\d\w\s]+)/(?P<page>\d+)?$', views.tag_list, name='tag_list'),
 ]
