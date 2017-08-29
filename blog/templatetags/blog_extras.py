fc849fbe74a243ecd1bf6eafd365d1b25ea99377
Ian Harper <ian@ianbharper.com>
2017-08-28 21:09:28 -0700

    Added more bootstrap styling to images and prev/nexk links.

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -8,3 +8,19 @@ def markdownify(text):
 	"""Convert markdown text to HTML"""
 	# import pdb; pdb.set_trace()
 	return markdown.markdown(text, safe_mode='escape')
+
+@register.filter
+def imageCaptions(text, img_pos=None):
+	if not(img_pos): img_pos = text.find('<img')
+	if img_pos > -1:
+		img_end = text.find('>', img_pos)
+		print(text[img_pos:img_end+1])
+		img_alt = ''
+		img_alt_pos = text.find('alt=', img_pos, img_end)
+		if img_alt_pos > -1: 
+			img_alt_end = text.find('"', img_alt_pos + 5)
+			img_alt = '<p class="caption text-center">' + text[img_alt_pos + 5:img_alt_end] + '</p>'
+		text  = text[:img_pos] + '<div class="thumbnail col-md-offset-3 center-thumbnail">' + text[img_pos:img_end+1] + img_alt + '</div>' + text[img_end+1:]
+		img_pos = text.find('<img', img_end)
+		if img_pos > -1: text = imageCaptions(text, img_pos)
+	return text
