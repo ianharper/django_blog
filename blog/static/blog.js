ebf9fe50e0ae2fd968bbab3206cdf0ad72f77e76
Ian Harper <ian@ianbharper.com>
2017-11-05 16:47:58 -0800

    Combined header and navbar

---
+++ blog/static/blog.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+
+//     $('#sidebarCollapse').on('click', function () {
+//     	// open/close sidebar
+//         $('#sidebar').toggleClass('active');
+//         // close dropdowns
+//         $('.collapse.in').toggleClass('in');
+//         // and also adjust aria-expanded attributes we use for the open/closed arrows
+//         // in our CSS
+//         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+//     });
+
+// });
