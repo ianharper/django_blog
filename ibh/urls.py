1f88cf29a7e56f70d0543fa16b489c461fba61d1
Ian Harper <ian@ianbharper.com>
2017-11-20 09:07:23 -0800

    Changed admin url.

--- ibh/urls.py
+++ ibh/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^ibhadmin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
 
